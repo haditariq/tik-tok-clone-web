6ba1a6120ef2244bbc582b6a4dafe8dee0718a2d
haditariq <hadi-tariq@live.co.uk>
2023-01-23 00:00:11 +0100

    update: tailwind added

---
+++ sanity-backend/schemas/index.ts
@@ -0,0 +1,12 @@
+import comment from "./comment";
+import post from "./post";
+import postedBy from "./postedBy";
+import user from "./user";
+
+export const schemaTypes = [
+  post,
+  comment,
+  postedBy,
+  user 
+]
+ 
